9aec75dfa61a52940f9b077256bc8f2172f2badf
Arjun <6112arjun@gmail.com>
2023-07-27 14:52:36 +0530

    Changed Backend url

--- React-webRTC/client/src/context/SocketProvider.jsx
+++ React-webRTC/client/src/context/SocketProvider.jsx
@@ -9,7 +9,10 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(
+    () => io("https://video-calling-backend.onrender.com"),
+    []
+  );
 
   return (
     <SocketContext.Provider value={socket}>
